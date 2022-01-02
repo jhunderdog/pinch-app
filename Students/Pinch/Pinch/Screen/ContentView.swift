40667fbf2158f76cf29fc3e5a5693abff8160a26
jhunderdog <33382water@gmail.com>
2022-01-02 17:15:25 +0900

    finished

--- Students/Pinch/Pinch/Screen/ContentView.swift
+++ Students/Pinch/Pinch/Screen/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
     @State private var imageOffset: CGSize = .zero
+    @State private var isDrawerOpen: Bool = false
     
     func resetImageState() {
         return withAnimation(.spring()) {
@@ -55,6 +56,27 @@ struct ContentView: View {
                                     resetImageState()
                                 }
                             }
+                    )
+                
+                //MARK: -3. MAGINIFICATION
+                    .gesture(
+                    MagnificationGesture()
+                        .onChanged({ value in
+                            withAnimation(.linear(duration: 1)) {
+                                if imageScale >= 1 && imageScale <= 5 {
+                                    imageScale = value
+                                } else if imageScale > 5 {
+                                    imageScale = 5
+                                }
+                            }
+                            
+                        }).onEnded { _ in
+                            if imageScale > 5 {
+                                imageScale = 5
+                            } else if imageScale <= 1 {
+                                resetImageState()
+                            }
+                        }
                     )
 //                    .animation(.linear(duration: 1), value: isAnimating)
             }
@@ -110,13 +132,42 @@ struct ContentView: View {
                         }
                     } //: CONTROLS
                     .padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 20))
+                    
                     .background(.ultraThinMaterial)
                     .cornerRadius(12)
                     .opacity(isAnimating ? 1 : 0)
                 }
                     .padding(.bottom, 30)
+                    
                 , alignment: .bottom
             )
+            //MARK: - DRAWER
+            .overlay(alignment: .topTrailing) {
+                HStack(spacing: 12) {
+                    //MARK : - DRAWER HANDLE
+                    Image(systemName: isDrawerOpen ? "chevron.compact.right" : "chevron.compact.left")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 40)
+                        .padding(8)
+                        .foregroundStyle(.secondary)
+                        .onTapGesture {
+                            withAnimation(.easeOut) {
+                                isDrawerOpen.toggle()
+                            }
+                        }
+                    Spacer()
+                    
+                    //MARK : - THUMBNAILS
+                }
+                .padding(EdgeInsets(top: 16, leading: 8, bottom: 16, trailing: 8))
+                .background(.ultraThinMaterial)
+                .cornerRadius(12)
+                .opacity(isAnimating ? 1 : 0)
+                .frame(width: 260)
+                .padding(.top, UIScreen.main.bounds.height / 12)
+                .offset(x: isDrawerOpen ? 20 : 215)
+            }
             
         }
         .navigationViewStyle(.stack)
